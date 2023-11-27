413034a61998a79dd7817be028eca17c0a553ada
Harini Allu <120156889+alluharini@users.noreply.github.com>
2023-11-27 14:45:28 +0530

    Update 10.maximum and minimum values of a vector.R

--- 10.maximum and minimum values of a vector.R
+++ 10.maximum and minimum values of a vector.R
@@ -1,14 +1,5 @@
-# Create a vector
-example_vector <- c(5, 3, 8, 2, 7, 1, 9, 4, 6)
-
-# Find the maximum and minimum values
-max_value <- max(example_vector)
-min_value <- min(example_vector)
-
-# Display the result
-print("Example Vector:")
-print(example_vector)
-print("Maximum Value:")
-print(max_value)
-print("Minimum Value:")
-print(min_value)
+nums = c(10, 20, 30, 40, 50, 60)
+print('Original vector:')
+print(nums)   
+print(paste("Maximum value of the said vector:",max(nums)))
+print(paste("Minimum value of the said vector:",min(nums)))

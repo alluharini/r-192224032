026ca8c38344d0c0519cb9c0838c8a8150b2a40e
Harini Allu <120156889+alluharini@users.noreply.github.com>
2023-11-27 14:11:15 +0530

    Add files via upload

---
+++ 18.3rows,3colums,2labels.R
@@ -0,0 +1,9 @@
+# Create two vectors
+vector1 <- c(1, 2, 3, 4, 5, 6, 7, 8, 9)
+vector2 <- c(10, 11, 12, 13, 14, 15, 16, 17, 18)
+
+# Create a 3D array with three columns, three rows, and two tables
+my_array <- array(c(vector1, vector2), dim = c(3, 3, 2))
+
+# Print the array
+print(my_array)

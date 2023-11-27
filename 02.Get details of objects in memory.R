268bfde999bff9af95941058c48949cf652c95fa
Harini Allu <120156889+alluharini@users.noreply.github.com>
2023-11-27 14:40:43 +0530

    Update 02.Get details of objects in memory.R

--- 02.Get details of objects in memory.R
+++ 02.Get details of objects in memory.R
@@ -1,6 +1,7 @@
-# Get details of objects in memory
-objects_info <- ls.str()
-
-# Display the details
-print("Details of Objects in Memory:")
-print(objects_info)
+name = "Python";
+n1 = 10;
+n2 = 0.5
+nums = c(10, 20, 30, 40, 50, 60)
+print(ls())
+print("Details of the objects in memory:")
+print(ls.str())

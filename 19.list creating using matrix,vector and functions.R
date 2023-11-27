498fc9edca51c5c6b40acfbdc791d11474545542
Harini Allu <120156889+alluharini@users.noreply.github.com>
2023-11-27 14:07:53 +0530

    Add files via upload

---
+++ 19.list creating using matrix,vector and functions.R
@@ -0,0 +1,9 @@
+# Create a list with vectors, matrices, and a function
+my_list <- list(
+  vector_element = c(1, 2, 3),
+  matrix_element = matrix(4:9, ncol = 2),
+  function_element = function(x) x^2
+)
+
+# Print the content of the list
+print(my_list)

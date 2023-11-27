1296df5a37f9405ed3e93e8a2fde917657d04db8
Harini Allu <120156889+alluharini@users.noreply.github.com>
2023-11-27 14:06:23 +0530

    Add files via upload

---
+++ 20.empty plot.R
@@ -0,0 +1,8 @@
+# Create an empty plot
+plot.new()
+
+# Create an empty plot with specified axes limits
+plot(1, type = "n", xlim = c(0, 10), ylim = c(0, 20), xlab = "X-axis", ylab = "Y-axis", main = "Empty Plot with Axes Limits")
+
+# Add a title to the plot
+title(main = "Empty Plot with Axes Limits")

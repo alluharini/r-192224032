fd3149c1845d40fe32d4be7c74ac74d4f19c390c
Harini Allu <120156889+alluharini@users.noreply.github.com>
2023-11-27 14:44:14 +0530

    Update 08.extract letters from a string.R

--- 08.extract letters from a string.R
+++ 08.extract letters from a string.R
@@ -1,16 +1,9 @@
-# Extract first 10 English letters in lower case
-first_10_lower <- letters[1:10]
-
-# Extract last 10 letters in upper case
-last_10_upper <- LETTERS[17:26]
-
-# Extract letters between 22nd to 24th letters in upper case
-letters_22_to_24_upper <- LETTERS[22:24]
-
-# Display the results
-print("First 10 English Letters in Lower Case:")
-print(first_10_lower)
-print("Last 10 Letters in Upper Case:")
-print(last_10_upper)
-print("Letters between 22nd to 24th in Upper Case:")
-print(letters_22_to_24_upper)
+print("First 10 letters in lower case:")
+t = (letters[1:10])
+print(t)
+print("Last 10 letters in upper case:")
+t = tail(LETTERS, 10)
+print(t)
+print("Letters between 22nd to 24th letters in upper case:")
+e = tail(LETTERS[22:24])
+print(e)

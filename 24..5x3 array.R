3702fd2c348f3b8b3703c9f8a6115a960267b744
Harini Allu <120156889+alluharini@users.noreply.github.com>
2023-11-27 13:59:08 +0530

    Add files via upload

---
+++ 24..5x3 array.R
@@ -0,0 +1,5 @@
+# Create a 5x3 array of even integers greater than 50
+even_sequence_matrix <- matrix(seq(52, by = 2, length.out = 5 * 3), nrow = 5, ncol = 3)
+
+# Print the resulting matrix
+print(even_sequence_matrix)

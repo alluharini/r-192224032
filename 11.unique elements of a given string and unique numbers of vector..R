98e7362f7614cacdfa46f093fde8439c6ce7b231
Harini Allu <120156889+alluharini@users.noreply.github.com>
2023-11-29 10:54:05 +0530

    Update 11.unique elements of a given string and unique numbers of vector..R

--- 11.unique elements of a given string and unique numbers of vector..R
+++ 11.unique elements of a given string and unique numbers of vector..R
@@ -1,17 +1,12 @@
-# Create a string
-example_string <- "programming"
+str1 = "The quick brown fox jumps over the lazy dog."
+print("Original vector(string)")
+print(str1)
+print("Unique elements of the said vector:")
+print(unique(tolower(str1)))
 
-# Get unique elements of the string
-unique_chars <- unique(strsplit(example_string, NULL)[[1]])
 
-# Create a vector
-example_vector <- c(1, 2, 3, 2, 4, 1, 5, 6, 3, 7)
-
-# Get unique elements of the vector
-unique_numbers <- unique(example_vector)
-
-# Display the results
-print("Unique Elements of the String:")
-print(unique_chars)
-print("Unique Elements of the Vector:")
-print(unique_numbers)
+nums = c(1, 2, 2, 3, 4, 4, 5, 6)
+print("Original vector(number)")
+print(nums)
+print("Unique elements of the said vector:")
+print(unique(nums))

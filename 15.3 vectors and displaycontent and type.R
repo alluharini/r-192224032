1bc6fc7af3ceae1ff7de9256436721837f211c29
Harini Allu <120156889+alluharini@users.noreply.github.com>
2023-11-29 10:56:10 +0530

    Update 15.3 vectors and displaycontent and type.R

--- 15.3 vectors and displaycontent and type.R
+++ 15.3 vectors and displaycontent and type.R
@@ -1,17 +1,9 @@
-# Create vectors
-numeric_vector <- c(1, 2, 3, 4, 5)
-character_vector <- c("apple", "blueberry", "banana")
-logical_vector <- c(TRUE, FALSE, TRUE, FALSE, TRUE)
-
-# Display content and type of vectors
-print("Numeric Vector:")
-print(numeric_vector)
-print(paste("Type:", typeof(numeric_vector)))
-
-print("Character Vector:")
-print(character_vector)
-print(paste("Type:", typeof(character_vector)))
-
-print("Logical Vector:")
-print(logical_vector)
-print(paste("Type:", typeof(logical_vector)))
+a = c(1, 2, 5, 3, 4, 0, -1, -3)
+b = c("Red", "Green", "White")
+c = c(TRUE, TRUE, TRUE, FALSE, TRUE, FALSE)
+print(a)
+print(typeof(a))
+print(b)
+print(typeof(b))
+print(c)
+print(typeof(c))

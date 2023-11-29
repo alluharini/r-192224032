e415c400ff157300b64dc4d23ac4be7eb38d2538
Harini Allu <120156889+alluharini@users.noreply.github.com>
2023-11-29 10:57:31 +0530

    Update 17.array,passing vectors and dimensions.R

--- 17.array,passing vectors and dimensions.R
+++ 17.array,passing vectors and dimensions.R
@@ -1,14 +1,10 @@
-# Create a vector of values
-values_vector <- c(1, 2, 3, 4, 5, 6, 7, 8, 9)
-
-# Create a vector of dimensions
-dimensions_vector <- c(3, 3)
-
-# Provide names for each dimension
-dim_names <- list(c("Row1", "Row2", "Row3"), c("Col1", "Col2", "Col3"))
-
-# Create an array with names for each dimension
-my_array <- array(values_vector, dim = dimensions_vector, dimnames = dim_names)
-
-# Print the array
-print(my_array)
+a =  array(
+  6:30,
+  dim = c(4, 3, 2),
+  dimnames = list(
+    c("Col1", "Col2", "Col3", "Col4"),
+    c("Row1", "Row2", "Row3"),
+    c("Part1", "Part2")
+  )
+)
+print(a)
